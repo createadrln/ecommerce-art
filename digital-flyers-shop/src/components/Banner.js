45b5875132a6a8ee83f8f2e7ca59afb2270f0bb0
Ryan MacMurray <ryan@createadrenaline.com>
2024-06-05 19:40:55 -0400

    Update Banner.js to 2 col

--- digital-flyers-shop/src/components/Banner.js
+++ digital-flyers-shop/src/components/Banner.js
@@ -4,8 +4,13 @@ import './Banner.css';
 function Banner() {
     return (
         <div className="banner">
-            <h1>Welcome to Digital Flyers & Invitations Shop!</h1>
-            <p>Your one-stop shop for all digital event needs!</p>
+            <div className="banner-text">
+                <h1>Welcome to Digital Flyers & Invitations Shop!</h1>
+                <p>Your one-stop shop for all digital event needs!</p>
+            </div>
+            <div className="banner-image">
+                <img src="path/to/your/image.jpg" alt="Banner" />
+            </div>
         </div>
     );
 }
